d417cc32edd7da6d39f62f80220778d8d400c54d
unstoppable840 <unstoppable840@gmail.com>
2024-03-02 19:57:54 +0800

    bug: 修复ios系统页面加载不出的bug

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,18 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .then(() => {
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
+    if(isIOS) {
+      setTimeout(() => {
+        document.getElementById('approot')!.style.display = '';
+        const bongoCat_dark = document.getElementById('bongoCat_dark');
+        const bongoCat_light = document.getElementById('bongoCat_light');
+        bongoCat_dark!.remove();
+        bongoCat_light!.remove();
+        document.getElementById('bongoCatCss')?.remove();
+      }, 500);
+      return;
+    }
     window.addEventListener('load', () => {
       document.getElementById('approot')!.style.display = '';
       const bongoCat_dark = document.getElementById('bongoCat_dark');
